49857e4f06ffd7ccd43a586cd6dde584c5530596
mantis78 <boon.tek@gmail.com>
2019-05-02 23:40:33 +0800

    Add a way to find all immediate directories.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::env;
+use std::{env, fs, io};
+use std::fs::DirEntry;
 
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let base_dir = match &args.get(1) {
@@ -8,5 +9,16 @@ fn main() {
         None => "."
     };
 
-    println!("{}", base_dir);
+    println!("Base Dir: {}", base_dir);
+
+    let dirs = fs::read_dir(base_dir).unwrap()
+        .filter_map(Result::ok)
+        .filter({
+            |e| e.file_type().unwrap().is_dir()
+        });
+
+    dirs.for_each(|e: DirEntry|
+        println!("dir: {}", e.file_name().to_str().unwrap()));
+
+    Ok(())
 }
