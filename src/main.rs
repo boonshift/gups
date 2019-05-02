0ef46e61ecb3ecf0b327c254d0a094150e762acf
mantis78 <boon.tek@gmail.com>
2019-05-03 00:09:15 +0800

    Got all to split to per thread and use channels.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{env, fs, io};
+use std::{env, fs, io, thread};
 use std::fs::DirEntry;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -12,10 +14,43 @@ fn main() -> io::Result<()> {
     println!("Base Dir: {}", base_dir);
 
     let dirs = get_dirs(base_dir);
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+    let mut children = Vec::new();
 
     for e in dirs {
-        println!("dir: {}", e.file_name().to_str().unwrap());
+//        println!("dir: {}", e.file_name().to_str().unwrap());
+        let thread_tx = tx.clone();
+
+        let child = thread::spawn(move || {
+            let dir_name_string = e.file_name();
+            let dir_name = dir_name_string.to_str().clone().unwrap();
+
+            // The thread takes ownership over `thread_tx`
+            // Each thread queues a message in the channel
+            let msg = format!("hello from {}", dir_name);
+            thread_tx.send(msg).unwrap();
+
+            // Sending is a non-blocking operation, the thread will continue
+            // immediately after sending its message
+//            println!("thread {} finished", dir_name);
+        });
+
+        children.push(child);
     }
+
+    let printer = thread::spawn(move || {
+        loop {
+            let msg = rx.recv().unwrap();
+            println!("Received: {}", msg);
+        }
+    });
+
+    // Wait for the threads to complete any remaining work
+    for child in children {
+        child.join().expect("oops! the child thread panicked");
+    }
+
+    let _ = printer.join();
 
     Ok(())
 }
