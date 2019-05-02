6975f9dd29f488026151d83a078ec73a158bc44b
mantis78 <boon.tek@gmail.com>
2019-05-02 22:52:21 +0800

    Add git lib. Add handling of at least 1 argument.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let base_dir = match &args.get(1) {
+        Some(v) => v,
+        None => "."
+    };
+
+    println!("{}", base_dir);
 }
